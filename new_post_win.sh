5e19b1510522998c635ed943a0bd33ad5db5570a
2han9wen71an <2han9wen71an@gmail.com>
2023-07-30 01:04:20 +0800

    添加win环境下创建文章

---
+++ new_post_win.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR="${0%\\*}"
+
+title=`echo $@ | sed 's/[ ][ ]*/-/g'`
+post_date=`date  +"%Y-%m-%d %T"`
+post_name="`date "+%Y-%m-%d"`-${title}.markdown"
+random_addr=`openssl rand -hex 8 | md5sum | cut -c1-8`
+
+cat > ${DIR}\\..\\_posts\\${post_name} << EOF
+---
+layout: post
+title:  "${title}"
+description: ""
+date:   ${post_date} +0800
+categories: 代码笔记 折腾笔记 生活随笔
+permalink: /CodeNotes Toss-notes LifeEssay/${random_addr}.html
+tags: [writing]
+---
+
+EOF
